b8d183bd68c52046608468406c3d7c89d418032b
Joshua Taylor <josh-taylor@uiowa.edu>
2021-01-03 20:08:25 -0500

    project init

---
+++ chatapp/chatapp/Controllers/ConverstaionsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatapp
+//
+//  Created by Joshua Taylor on 1/3/21.
+//
+
+import UIKit
+
+class ConverstaionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
